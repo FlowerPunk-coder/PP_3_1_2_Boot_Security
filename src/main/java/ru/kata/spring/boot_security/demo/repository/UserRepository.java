320a5f436d64522632151b7e9d0e0e35aad1d478
Max <maxim.ketikov@gmail.com>
2022-08-19 22:34:45 +0300

    3.1.2

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -1,14 +1,17 @@
 package ru.kata.spring.boot_security.demo.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.Optional;
 
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("from User u where u.username = ?1")
-    Optional<User> findByUsername(String username);
+    @EntityGraph(value = "User.role", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select u from User u where u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
 }
